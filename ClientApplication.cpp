e09e5d21465fb3de8e0c3ea7d44ed3bd215fa8e1
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2019-04-17 22:19:50 -0300

    Send message if host name was found

--- ClientApplication.cpp
+++ ClientApplication.cpp
@@ -4,21 +4,27 @@
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
-        printf("no host name");
+        printf("no host name\n");
         return -1;
     }
 
     ClientSocketWrapper clientSocket(argv[1], SocketWrapper::DEFAULT_PORT);
-    
-    printf("Enter the message: ");
-    char input[256] = "";
-    fgets(input, 256, stdin);
 
-    clientSocket.writeToServer(string(input));
+    if (clientSocket.foundHostName == true) {
+        printf("Enter the message: ");
+        char input[256] = "";
+        fgets(input, 256, stdin);
 
-    string answer = clientSocket.readFromServer();
-    printf("%s\n", answer.c_str());
-    
-	clientSocket.closeSocket();
+        clientSocket.writeToServer(string(input));
+
+        string answer = clientSocket.readFromServer();
+        printf("%s\n", answer.c_str());
+
+        clientSocket.closeSocket();
+    } else {
+       printf("Cannot connect\n");
+       return -1;
+    }
+
     return 0;
 }
