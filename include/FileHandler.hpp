1fd1c334cbbe1b23de6d072a7fdf77e0398bce30
camargodev <camargodev@gmail.com>
2019-04-30 13:56:22 -0300

    Functions to list files

--- include/FileHandler.hpp
+++ include/FileHandler.hpp
@@ -1,6 +1,8 @@
 #ifndef FILE_HANDLER_HPP
 #define FILE_HANDLER_HPP
 
+#include <dirent.h>
+#include <vector> 
 #include "FileWrapper.hpp"
 
 class FileHandler {
@@ -9,8 +11,13 @@ public:
     void deleteFile(char* filename);
     void createFile(char* filename, char* content);
     int getFileSize(const char* filename);
+    vector<string> getFilenamesInDir(char* dirName);    
 
 private:
     string path;
+    const int FILE_TYPE = 8;
+
+    bool isFilenameValid(char* archiveName);
+    bool isFile(int archiveType);
 };
 #endif
--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -13,4 +13,28 @@ int FileHandler :: getFileSize(const char* filename) {
     return in.tellg(); 
 }
 
+vector<string> FileHandler :: getFilenamesInDir(char* dirName) {
+    struct dirent *currentFile;
+    vector<string> filenames;
+    DIR *directory = opendir(dirName);
+
+    while((currentFile = readdir(directory)) != NULL)
+        if (isFilenameValid(currentFile->d_name) && isFile(currentFile->d_type))
+            filenames.push_back(currentFile->d_name);
+    
+    closedir(directory);
+
+    return filenames;
+}
+
+bool FileHandler :: isFilenameValid(char* archiveName) {
+    string strArch = string(archiveName);
+    return strArch.compare(".") != 0 
+        && strArch.compare("..") != 0;
+}
+
+bool FileHandler :: isFile(int archiveType) {
+    return archiveType == FILE_TYPE;
+}
+
 
