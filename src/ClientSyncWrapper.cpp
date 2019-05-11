4aaefe51dece3551ff25d3c4df5cc2fcd6c92a16
Lucio <lucio.lpf@gmail.com>
2019-05-11 20:22:45 -0300

    SYNC - Adiciona logica pra reconhecer eventos

--- src/ClientSyncWrapper.cpp
+++ src/ClientSyncWrapper.cpp
@@ -1,57 +1,45 @@
 #include "../include/ClientSyncWrapper.hpp"
 #include <sys/inotify.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 #define EVENT_SIZE  (sizeof (struct inotify_event))
 
-/* reasonable guess as to size of 1024 events */
-#define BUF_LEN (1024 * (EVENT_SIZE + 16))
+#define BUF_LEN (EVENT_SIZE + 4096)
 
-static void displayInotifyEvent(struct inotify_event *i)
-{
-    cout << "====================================" << endl;
-    cout << "wd = " << i->wd << endl;
-    if (i->cookie > 0)
-        cout << "cookie = " << i->cookie << endl;
+static void dealWithEvent(struct inotify_event *event){
+    if (event->len != 32){
+        cout << "Mask: " << event->mask << endl;
+        cout << "Len: " << event->len << endl;
+        cout << "Name: " << event->name << endl;
+        string swp = "swp";
+        string name = event->name;
+        if (name.find(swp) != std::string::npos){
+            cout << "eh swp"<<endl;
+        }
+        
+    }
 
-    cout << "mask = ";
-    if (i->mask & IN_ACCESS)        cout << "IN_ACCESS ";
-    if (i->mask & IN_ATTRIB)        cout << "IN_ATTRIB ";
-    if (i->mask & IN_CLOSE_NOWRITE) cout << "IN_CLOSE_NOWRITE ";
-    if (i->mask & IN_CLOSE_WRITE)   cout << "IN_CLOSE_WRITE ";
-    if (i->mask & IN_CREATE)        cout << "IN_CREATE ";
-    if (i->mask & IN_DELETE)        cout << "IN_DELETE ";
-    if (i->mask & IN_DELETE_SELF)   cout << "IN_DELETE_SELF ";
-    if (i->mask & IN_IGNORED)       cout << "IN_IGNORED ";
-    if (i->mask & IN_ISDIR)         cout << "IN_ISDIR ";
-    if (i->mask & IN_MODIFY)        cout << "IN_MODIFY ";
-    if (i->mask & IN_MOVE_SELF)     cout << "IN_MOVE_SELF ";
-    if (i->mask & IN_MOVED_FROM)    cout << "IN_MOVED_FROM ";
-    if (i->mask & IN_MOVED_TO)      cout << "IN_MOVED_TO ";
-    if (i->mask & IN_OPEN)          cout << "IN_OPEN ";
-    if (i->mask & IN_Q_OVERFLOW)    cout << "IN_Q_OVERFLOW ";
-    if (i->mask & IN_UNMOUNT)       cout << "IN_UNMOUNT ";
-    else count << i->mask;
-    count << endl;
 
-    if (i->len > 0)
-        cout << "name = " << i->name << endl;
 }
+
 void ClientSyncWrapper::checkForUpdates() {
     int fd = inotify_init1(IN_NONBLOCK);
-    int wd = inotify_add_watch(fd, "/home/lucio/monitora", IN_MODIFY);
-    char buffer[BUF_LEN];
+    int wd = inotify_add_watch(fd, "/home/lucio/monitora", IN_CLOSE_WRITE | IN_MOVED_FROM | IN_MOVED_TO);
+    char *buffer;
 
     int size_read, i;
     while(true) {
         i = 0;
         size_read = read(fd, buffer, BUF_LEN);
         if(size_read > 0) {
-                inotify_event* event = (struct inotify_event*) &buffer[i];
-                displayInotifyEvent(event);
-                i += event->len + EVENT_SIZE;
+
+            inotify_event* event = (struct inotify_event*) buffer;
+            dealWithEvent(event);
+            i += event->len + EVENT_SIZE;
+
         }
     }
 
